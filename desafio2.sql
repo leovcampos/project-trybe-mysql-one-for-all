be96793c210fcf45359a65072efb1d3d289aee03
leovcampos <leonardoviniciuscampos@gmail.com>
2022-09-26 16:04:55 -0300

    req 02 corrigido

--- desafio2.sql
+++ desafio2.sql
@@ -3,8 +3,8 @@ SELECT
   COUNT(DISTINCT art.artist_id) AS artistas,
   COUNT(DISTINCT alb.album_id) AS albuns
 FROM
-  SpotifyClone.musics AS spo
-  INNER JOIN SpotifyClone.albums AS alb
+  SpotifyClone.Track AS spo
+  INNER JOIN SpotifyClone.Album AS alb
   ON al.album_id = m.album_id
-  INNER JOIN SpotifyClone.artists AS art
+  INNER JOIN SpotifyClone.Artist AS art
   ON ar.artist_id = al.artist_id;
