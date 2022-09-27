9049c677f7c9969a92237cb73740360a3665fe6c
leovcampos <leonardoviniciuscampos@gmail.com>
2022-09-27 13:39:06 -0300

    req 06

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT
+    MIN(Plan.plan_value) AS 'faturamento_minimo'
+    MAX(Plan.plan_value) AS 'faturamento_maximo'
+    ROUND(AVG(Plan.plan_value), 2) AS 'faturamento_medio'
+    SUM(Plan.plan_value) AS 'faturamento_total'
+FROM
+    SpotifyClone.User User
+INNER JOIN
+    SpotifyClone.Plan Plan
+ON
+    Plan.plan_id = User.plan_id;
