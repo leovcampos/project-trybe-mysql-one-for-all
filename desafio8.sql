19f1e591999645cb4749ce686bac2330805add06
leovcampos <leonardoviniciuscampos@gmail.com>
2022-09-27 14:15:42 -0300

    req 08

--- desafio8.sql
+++ desafio8.sql
@@ -7,9 +7,5 @@ INNER JOIN
     SpotifyClone.Album Alb
 ON
     Alb.artist_id = Art.artist_id
-GROUP BY
-    Art.artist_name
 HAVING
-    Art.artist_name = 'Elis Regina'
-ORDER BY
-    album;
+    Art.artist_name = 'Elis Regina';
